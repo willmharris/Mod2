3ba4c4323084ae31723d88c60389e421442c81c5
willmharris <willmharris0@gmail.com>
2020-03-06 12:17:47 -0500

    added differentiated backgrounds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 DatabaseCleaner.clean_with(:truncation)
 
 Dm.create(name: "Will", password: "123", email: "willmharris0@gmail.com")
-Dm.create(name: "Bob", password: "123", email: "bobman@gmail.com")
+Dm.create(name: "Bob", password: "123", email: "bobisgreat@gmail.com")
+Dm.create(name: "Wesley", password: "123", email: "bestemail@gmail.com")
 Monster.create(name: "Goblin", popularity: 0, creator_id: 1, size: "Small", category: "Humanoid", alignment: "Neutral evil", ac: 15, hp: 7, speed: 30, strength: 8, dexterity: 14, constitution: 10, intelligence: 10, wisdom: 8, charisma: 8, cr: 1, image: "https://i.pinimg.com/originals/b4/74/9a/b4749aa294065dcf812f68f5f27fb46b.png" )
 DmMonster.create(dm_id: 1, monster_id: 1)
 Action.create(monster_id: 1, name: "Scimitar", description: "Melee Weapon Attack: +4 to hit, reach 5 ft., one target. Hit: (1d6 + 2) slashing damage.")
@@ -18,3 +19,11 @@ Action.create(monster_id: 3, name: "Claw", description: "Melee Weapon Attack: +1
 Action.create(monster_id: 3, name: "Tail", description: "Melee Weapon Attack: +14 to hit, reach 15 ft., one target. Hit: (2d8 + 8) bludgeoning damage.")
 Action.create(monster_id: 3, name: "Frightful Presence", description: "Each creature of the dragon's choice that is within 120 ft. of the dragon and aware of it must succeed on a DC 19 Wisdom saving throw or become frightened for 1 minute. A creature can repeat the saving throw at the end of each of its turns, ending the effect on itself on a success. If a creature's saving throw is successful or the effect ends for it, the creature is immune to the dragon's Frightful Presence for the next 24 hours.")
 Action.create(monster_id: 3, name: "Fire Breath", description: "The dragon exhales fire in a 60-foot cone. Each creature in that area must make a DC 21 Dexterity saving throw, taking 63 (18d6) fire damage on a failed save, or half as much damage on a successful one.")
+Monster.create(name: "Big Boy: The Chicken", creator_id: 3, size: "Gargantuan", category: "Beast", alignment: "Chaotic Evil", ac: 10, hp: 15, speed: 3, strength: 10, dexterity: 10, constitution: 5, intelligence: 0, wisdom: 0, charisma: 30, cr: 1, image: "https://i.pinimg.com/564x/c0/e7/e7/c0e7e7cf5202ef4dfe584cae920aa6f2.jpg", popularity: 2)
+DmMonster.create(dm_id: 1, monster_id: 4)
+DmMonster.create(dm_id: 2, monster_id: 4)
+DmMonster.create(dm_id: 3, monster_id: 4)
+Action.create(monster_id: 4, name: "Peck", description: "Destroys a skyscraper.")
+Action.create(monster_id: 4, name: "Lay Egg", description: "Prepares an egg." )
+Action.create(monster_id: 4, name: "Egg Hatch", description: "Creates a small boy." )
+
